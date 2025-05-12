c383599ef266df26619f445dbdbda124834cb2a6
Fabio Souza Miranda <fabiosouza21777@gmail.com>
2025-05-12 12:32:35 -0300

    repositories

---
+++ boot/src/main/java/com/fsm/security/repositories/UserRoleJdbcRepository.java
@@ -0,0 +1,22 @@
+package com.fsm.security.repositories;
+
+import com.fsm.security.entities.UserRole;
+import com.fsm.security.entities.UserRoleId;
+import io.micronaut.data.annotation.Query;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.repository.CrudRepository;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@JdbcRepository(dialect = Dialect.POSTGRES)
+public interface UserRoleJdbcRepository extends CrudRepository<UserRole, UserRoleId> { // <2>
+
+    @Query("""
+    SELECT authority FROM role 
+    INNER JOIN user_role ON user_role.id_role_id = role.id 
+    INNER JOIN "user" user_ ON user_role.id_user_id = user_.id 
+    WHERE user_.username = :username""")
+    List<String> findAllAuthoritiesByUsername(@NotBlank String username);
+}
